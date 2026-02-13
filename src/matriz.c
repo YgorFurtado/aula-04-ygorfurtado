9418999336401058d173619e34005738be2f8f75
Ygor Furtado <88071482+YgorFurtado@users.noreply.github.com>
2026-02-13 14:23:11 +0000

    second gommit

--- src/matriz.c
+++ src/matriz.c
@@ -1,5 +1,6 @@
 #include "matriz.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 struct Matriz{
     double** data;
@@ -13,11 +14,44 @@ Matriz* matriz_create(size_t rows, size_t cols){
 
     matriz->data = malloc(sizeof(double*)* rows);
     for (size_t i = 0; i < rows; i++) {
-        matriz->data[i] = malloc(sizeof(double*)* cols);       
+        matriz->data[i] = calloc(cols, sizeof(double*));       
     }
 
     matriz->rows = rows;
     matriz->cols = cols;
 
     return matriz;
+}
+
+void matriz_destroy(Matriz* matriz){
+    for (size_t i = 0;i < matriz->rows; i++){
+        free(matriz->data[i]);
+    }
+    free(matriz->data);
+    free(matriz);
+}
+
+void matriz_print(const Matriz* matriz) {
+    for (size_t i = 0; i < matriz->rows; i++){
+        for (size_t j = 0; j < matriz->rows; j++){
+            printf("%.2f ", matriz->data[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+double matriz_get(Matriz* matriz, size_t row, size_t col){
+    return matriz->data[row][col];
+}
+
+void matriz_set(Matriz* matriz, size_t row, size_t col, double value){
+    matriz->data[row][col] = value;
+}
+
+size_t matriz_rows(Matriz* matriz){
+    return matriz->rows;
+}
+
+size_t matriz_cols(Matriz* matriz){
+    return matriz->cols;
 }
--- src/matriz.h
+++ src/matriz.h
@@ -7,6 +7,7 @@ typedef struct Matriz Matriz;
 
 Matriz* matriz_create(size_t rows, size_t cols);
 void matriz_destroy(Matriz* matriz);
+void matriz_print(const Matriz* matriz);
 double matriz_get(Matriz* matriz, size_t rows, size_t cols);
 void matriz_set(Matriz* matriz, size_t row, size_t col, double value);
 size_t matriz_rows(Matriz* matriz);
